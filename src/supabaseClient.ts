8bd03ccde0efb0add8e3b5116d84e5e840c7d1c8
SafoanMiah <saf.home.2017@gmail.com>
2025-02-01 06:38:34 +0000

    env vite removal

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://qnplpbvvpbfgfcbchjmd.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFucGxwYnZ2cGJmZ2ZjYmNoam1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNjcxODksImV4cCI6MjA1Mzg0MzE4OX0.ygZate-knXeAZ4P8_DTzTbdic85zuGjvKDjEncnsB1k"
 
 console.log('Supabase URL:', SUPABASE_URL);
 console.log('Supabase Anon Key:', SUPABASE_ANON_KEY);
